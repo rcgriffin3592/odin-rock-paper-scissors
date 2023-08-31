9dea81f5168bcd3e986788212c04a1d521ca2d1b
Ryan Griffin <rcgriffin3592@gmail.com>
2023-08-31 11:19:47 -0400

    Create getComputerChoice function

---
+++ script.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+
+    if (randomNumber === 0) {
+        return "Rock";
+    } else if (randomNumber === 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
