5d05bf38c027d811b479baa842caff13d610dcc7
Ryan Griffin <rcgriffin3592@gmail.com>
2023-08-31 11:48:17 -0400

    Create and test playRound function

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ function getComputerChoice() {
         return "Scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    let playerChoice = playerSelection.toLowerCase();
+
+    if (playerChoice === "rock" && computerSelection === "Scissors" || playerChoice === "scissors" && computerSelection === "Paper" || playerChoice === "paper" 
+    && computerSelection === "Rock") {
+        return `You Win! ${playerChoice} beats ${computerSelection}.`;
+    } else if (playerChoice === computerSelection.toLowerCase()) {
+        return `Draw! You both chose ${playerChoice}`;
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerChoice}`;
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
